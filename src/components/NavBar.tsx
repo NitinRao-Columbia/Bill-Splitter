5dbb37e5d7ced12ec9cfa57ac0f92e033154dae1
evageo <126923666+evageo@users.noreply.github.com>
2024-10-29 16:05:16 -0400

    centering

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,18 @@
 import { NavLink } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import './NavBar.css'; // Import custom CSS for additional styling
-import '../App';
+import './NavBar.css';
+import '../App.css';
 
 const NavBar = () => {
     return (
         <Navbar bg="primary" variant="dark" expand="lg" sticky="top" className="shadow">
-            <Container>
-                <Navbar.Brand href="/" className="fw-bold text-white">
+            <Container className="navbar-container">
+                <Navbar.Brand className="navbar-title fw-bold">
                     Bill Splitter
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mx-auto">
+                    <Nav className="navbar-links">
                         <Nav.Link as={NavLink} to="/billsplitter" className="nav-link">
                             Bill Splitter
                         </Nav.Link>
